5a341881f132090bf128218d24f6cfaaa4eb76d8
omargawdat <omargawdaat@gmail.com>
2024-04-25 01:01:33 +0200

    refactor: admin and business layer

--- apps/template/apps.py
+++ apps/template/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AppOneConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.template"
+
+    def ready(self):
+        from .admin import admin  # noqa
