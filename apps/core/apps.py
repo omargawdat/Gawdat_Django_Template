bd3f100de327a0dbe18bba8d59c870d8e1ee03a8
omargawdat <omargawdaat@gmail.com>
2024-05-02 21:30:37 +0300

    feat: load all the models files

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -9,9 +9,20 @@ class CoreConfig(AppConfig):
     name = "apps.core"
 
     def ready(self):
+        # import admins
         for app_config in apps.get_app_configs():
-            admin_path = os.path.join(app_config.path, 'admin')
-            if os.path.exists(admin_path):
-                admin_modules = [f[:-3] for f in os.listdir(admin_path) if f.endswith('.py') and not f.startswith('__')]
-                for module in admin_modules:
-                    importlib.import_module(f"{app_config.name}.admin.{module}")
+            self.import_modules_from_directory(app_config.path, "admin", app_config.name)
+
+        # import signals
+        self.import_modules_from_directory(self.path, "signals", self.name)
+
+        # Import models
+        for app_config in apps.get_app_configs():
+            self.import_modules_from_directory(app_config.path, "models", app_config.name)
+
+    def import_modules_from_directory(self, base_path, subdirectory, app_name):
+        directory_path = os.path.join(base_path, subdirectory)
+        if os.path.exists(directory_path):
+            modules = [f[:-3] for f in os.listdir(directory_path) if f.endswith(".py") and not f.startswith("__")]
+            for module in modules:
+                importlib.import_module(f"{app_name}.{subdirectory}.{module}")
