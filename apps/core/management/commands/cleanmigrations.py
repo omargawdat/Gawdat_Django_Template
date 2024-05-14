53b675a1735143d3a20b0bfcceace9e92ae79b0a
omargawdat <omargawdaat@gmail.com>
2024-05-14 19:16:37 +0300

    checkout

--- apps/core/management/commands/cleanmigrations.py
+++ apps/core/management/commands/cleanmigrations.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
     help = "Cleans all migration files in all apps, clears the django_migrations table, and then re-creates and fakes them."
 
     def handle(self, *args, **kwargs):
-        # Step 0: Make migrations and migrate to ensure that the database is up-to-date
-        call_command("makemigrations")
-        call_command("migrate")
-        self.stdout.write(self.style.SUCCESS("Database is up-to-date."))
+        # # Step 0: Make migrations and migrate to ensure that the database is up-to-date
+        # call_command("makemigrations")
+        # call_command("migrate")
+        # self.stdout.write(self.style.SUCCESS("Database is up-to-date."))
 
         # Step 1: Delete all migration records from the django_migrations table
         self.stdout.write(self.style.WARNING("Deleting all records from django_migrations table..."))
@@ -51,7 +51,7 @@ class Command(BaseCommand):
         self.stdout.write(self.style.WARNING("Recreating migration files..."))
         call_command("makemigrations")
 
-        # Step 4: Fake apply all migrations
-        self.stdout.write(self.style.WARNING("Applying all migrations as fake..."))
-        call_command("migrate", "--fake")
-        self.stdout.write(self.style.SUCCESS("All migrations have been faked."))
+        # # Step 4: Fake apply all migrations
+        # self.stdout.write(self.style.WARNING("Applying all migrations as fake..."))
+        # call_command("migrate", "--fake")
+        # self.stdout.write(self.style.SUCCESS("All migrations have been faked."))
