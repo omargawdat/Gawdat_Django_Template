8b459354736a3db7b2fa41da9aedee788dd149fc
omargawdat <omargawdaat@gmail.com>
2024-07-14 00:11:33 +0300

    feat: add template app

--- config/api_router.py
+++ config/api_router.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
-from template.users.api.views import UserViewSet
-
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 app_name = "api"
 urlpatterns = router.urls
--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.views import defaults as default_views
-from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
-from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
@@ -19,7 +16,6 @@ urlpatterns = [
 urlpatterns += [
     # API base url
     path("api/", include("config.api_router")),
-
     # DRF auth token
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
@@ -30,6 +26,6 @@ urlpatterns += [
 ]
 
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+    import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
