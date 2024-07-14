ea4f06a86b0deaf8cf78b396780121bc078a04e0
omargawdat <omargawdaat@gmail.com>
2024-07-15 00:10:11 +0300

    chore: add files

--- apps/users/domain/validators/date.py
+++ apps/users/domain/validators/date.py
@@ -4,9 +4,7 @@ from apps.common.mixins.validators import BaseValidator
 
 class DateValidator(BaseValidator):
     @staticmethod
-    def validate_date_format(
-        date_str, date_format="%Y-%m-%d", use_drf=ValidationErrorType.DRF
-    ):
+    def validate_date_format(date_format="%Y-%m-%d", use_drf=ValidationErrorType.DRF):
         DateValidator.raise_error(
             f"Invalid date format, should be {date_format}", use_drf
         )
