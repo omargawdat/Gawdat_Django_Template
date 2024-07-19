ad6fbad399011b639b4f432504b1f1a65bb42b39
omargawdat <omargawdaat@gmail.com>
2024-07-19 13:53:29 +0300

    feat: add jwt token generator

---
+++ apps/users/domain/utilities/jwt.py
@@ -0,0 +1,11 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class JWT:
+    @staticmethod
+    def generate_tokens_for_user(user):
+        refresh = RefreshToken.for_user(user)
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
