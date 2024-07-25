b1d40f45f35e4260accb54f2799652a53019910f
omargawdat <omargawdaat@gmail.com>
2024-07-25 22:32:52 +0300

    feat: add search_help_text into the template

--- common/boilerplate/admin.py
+++ common/boilerplate/admin.py
@@ -12,13 +12,14 @@ class CustomAdminClassBase(BaseModelAdmin):
     #  List View
     # -----------------------------------------------------------------------------------------
     list_display = ()
-    search_fields = ()
     list_filter = ()
     date_hierarchy = None
     list_per_page = 50
     list_filter_submit = False
     list_fullwidth = False
     list_horizontal_scrollbar_top = False
+    search_fields = ()
+    search_help_text = ""
 
     def get_ordering(self, request):
         return ()
