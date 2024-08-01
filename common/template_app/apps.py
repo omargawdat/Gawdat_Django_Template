17ce6ae1634fb8a79c80a816b72551ff0a440436
omargawdat <omargawdaat@gmail.com>
2024-08-01 13:49:31 +0300

    feat: update tempalte app

--- common/template_app/apps.py
+++ common/template_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TemplateConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "common.template_app"
+    name = "apps.template_app"
