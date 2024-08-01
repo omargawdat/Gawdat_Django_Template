3582d94b93407e3e964d39923f40f7906f60fead
omargawdat <omargawdaat@gmail.com>
2024-08-01 17:04:20 +0300

    update template app

--- common/template_app/admin/admin_model/admin.py
+++ common/template_app/admin/admin_model/admin.py
@@ -1,61 +1,57 @@
-from django.contrib import admin
-
-from common.base.basemodeladmin import BaseModelAdmin
-
-
-class MyModel:
-    pass
-
-
-@admin.register(MyModel)
-class CustomAdminClassBase(BaseModelAdmin):
-    #  List View
-    # -----------------------------------------------------------------------------------------
-    list_display = ()
-    list_filter = ()
-    date_hierarchy = None
-    list_per_page = 50
-    list_filter_submit = False
-    list_fullwidth = False
-    list_horizontal_scrollbar_top = False
-    search_fields = ()
-    search_help_text = ""
-
-    def get_ordering(self, request):
-        return ()
-
-    # Change View
-    # -----------------------------------------------------------------------------------------
-    form = None
-    filter_horizontal = ()
-    compressed_fields = False
-    autocomplete_fields = ()
-
-    def get_fieldsets(self, request, obj=None):
-        return (
-            ("Name", {"fields": ("field_one, field_two")}),
-            ("Name", {"fields": ("field_one, field_two")}),
-        )
-
-    def get_readonly_fields(self, request, obj=None):
-        return ()
-
-    def get_inlines(self, request, obj=None):
-        if obj is None:
-            return []
-        return []
-
-    # Permissions
-    # -----------------------------------------------------------------------------------------
-
-    def has_delete_permission(self, request, customer=None):
-        return False
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    # Other
-    # -----------------------------------------------------------------------------------------
+# from django.contrib import admin
+#
+# from common.base.basemodeladmin import BaseModelAdmin
+#
+#
+# @admin.register(MyModel)
+# class CustomAdminClassBase(BaseModelAdmin):
+#     #  List View
+#     # -----------------------------------------------------------------------------------------
+#     list_display = ()
+#     list_filter = ()
+#     date_hierarchy = None
+#     list_per_page = 50
+#     list_filter_submit = False
+#     list_fullwidth = False
+#     list_horizontal_scrollbar_top = False
+#     search_fields = ()
+#     search_help_text = ""
+#
+#     def get_ordering(self, request):
+#         return ()
+#
+#     # Change View
+#     # -----------------------------------------------------------------------------------------
+#     form = None
+#     filter_horizontal = ()
+#     compressed_fields = False
+#     autocomplete_fields = ()
+#
+#     def get_fieldsets(self, request, obj=None):
+#         return (
+#             ("Name", {"fields": ("field_one, field_two")}),
+#             ("Name", {"fields": ("field_one, field_two")}),
+#         )
+#
+#     def get_readonly_fields(self, request, obj=None):
+#         return ()
+#
+#     def get_inlines(self, request, obj=None):
+#         if obj is None:
+#             return []
+#         return []
+#
+#     # Permissions
+#     # -----------------------------------------------------------------------------------------
+#
+#     def has_delete_permission(self, request, customer=None):
+#         return False
+#
+#     def has_add_permission(self, request):
+#         return False
+#
+#     def has_change_permission(self, request, obj=None):
+#         return False
+#
+#     # Other
+#     # -----------------------------------------------------------------------------------------
---
+++ common/template_app/admin/admin_model/form.py
@@ -0,0 +1,7 @@
+# from django import forms
+#
+#
+# class ProviderForm(forms.ModelForm):
+#     class Meta:
+#         model = Model
+#         fields = "__all__"
