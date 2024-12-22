e04e82624325b6eb522040f5852aeed0ca4ede07
Omar Gawdat <omargawdaat@gmail.com>
2024-12-22 22:28:20 +0200

    feat: make the validation of the endpoints only inside the restframework

--- apps/users/other/api_auth.py
+++ apps/users/other/api_auth.py
@@ -5,18 +5,16 @@ from rest_framework.exceptions import AuthenticationFailed
 
 
 class AuthenticationFailedMessages:
-    INVALID_API_KEY = "Invalid or missing X-API-Key header"
+    INVALID_API_KEY = "Invalid or missing X-API-Key header"  # pragma: allowlist secret
 
 
 class APIKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         path = request.path_info.lower()
 
-        # Skip check for paths containing 'web' or 'callback'
         if "web" in path or "callback" in path:
             return
 
-        # pragma: allowlist secret
         api_key = request.headers.get("x-api-key")
         expected_api_key = os.environ.get("API_KEY")
 
