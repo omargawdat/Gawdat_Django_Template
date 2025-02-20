9c3df104d5c07491b654b4e619e0c7d411974d36
Omar Gawdat <omargawdaat@gmail.com>
2025-02-20 20:48:07 +0200

    feat: load aws secrets

--- config/helpers/env.py
+++ config/helpers/env.py
@@ -1,3 +1,24 @@
+import json
+import os
+
+import boto3
+
 from config.env_settings import EnvSettings
 
+
+def load_aws_secrets():
+    """Load AWS Secrets into the environment variables if they exist"""
+    region = os.getenv("AWS_SECRETS_REGION")
+    secret_id = os.getenv("AWS_SECRET_ID")
+    if not region or not secret_id:
+        return
+
+    client = boto3.Session().client("secretsmanager", region_name=region)
+    response = client.get_secret_value(SecretId=secret_id)
+    secrets_dict = json.loads(response["SecretString"])
+    for key, value in secrets_dict.items():
+        os.environ[key.lower()] = str(value)
+
+
+load_aws_secrets()
 env = EnvSettings()
