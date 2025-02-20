6cb9ce072f9ccf0273e2e8d68480d6be1fdac6d9
Omar Gawdat <omargawdaat@gmail.com>
2025-02-20 20:16:31 +0200

    feat: pass environment into the container

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,15 +8,9 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 sys.path.append(str(BASE_DIR / "apps"))
 
-# Choose settings based on environment variable
-print(f"ENVIRONMENT: {os.environ.get('ENVIRONMENT')}")
-if os.environ.get("ENVIRONMENT") in ["dev", "prod", "stag"]:
+if os.environ.get("ENVIRONMENT") in ["development", "production", "staging"]:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 elif os.environ.get("ENVIRONMENT") == "local":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-else:
-    raise ValueError(
-        f"Invalid or missing DJANGO_ENV. Got: {os.environ.get('DJANGO_ENV')}"
-    )
 
 application = get_wsgi_application()
