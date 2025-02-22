a923216fedd86a2ddfc6c2e70235885d59d0b14d
Omar Gawdat <omargawdaat@gmail.com>
2025-02-22 10:45:48 +0200

    fix: warning in swagger

--- config/integrations/drf.py
+++ config/integrations/drf.py
@@ -6,7 +6,6 @@ from config.helpers.env import env
 REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "drf_standardized_errors.handler.exception_handler",
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "apps.users.other.api_auth.APIKeyAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
