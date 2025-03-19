80eeafe388d017496a38f3150a8a5de0f74b161b
Omar Gawdat <omargawdaat@gmail.com>
2025-03-19 12:02:28 +0200

    fix: set the django configuration inside mange.py

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # ruff: noqa
+import os
 import sys
 from pathlib import Path
 
@@ -21,5 +22,10 @@ if __name__ == "__main__":
     # This allows easy placement of apps within the interior
     current_path = Path(__file__).parent.resolve()
     sys.path.append(str(current_path / "apps"))
+
+    is_local = os.environ.get("IS_LOCAL", "true").lower() == "true"
+    os.environ["DJANGO_SETTINGS_MODULE"] = (
+        "config.settings.local" if is_local else "config.settings.prod"
+    )
 
     execute_from_command_line(sys.argv)
