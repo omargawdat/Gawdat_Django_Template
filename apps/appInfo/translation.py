1d6974d80b2af975814611bfa84f38675d4cd885
Moataz0000 <moatazfawzy150@gmail.com>
2025-07-31 11:24:58 +0300

    feat: translate onboarding

--- apps/appInfo/translation.py
+++ apps/appInfo/translation.py
@@ -3,6 +3,7 @@ from modeltranslation.translator import register
 
 from apps.appInfo.models.app_info import AppInfo
 from apps.appInfo.models.faq import FAQ
+from apps.appInfo.models.onboarding import Onboarding
 
 
 @register(AppInfo)
@@ -13,3 +14,9 @@ class AppInfoTranslationOptions(TranslationOptions):
 @register(FAQ)
 class FAQTranslationOptions(TranslationOptions):
     fields = ("question", "answer")
+
+
+@register(Onboarding)
+class OnboardingTranslationOptions(TranslationOptions):
+    fields = ("title", "text", "sub_text")
+    required_languages = ("en", "ar")
