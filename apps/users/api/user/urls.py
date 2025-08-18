b41e13de1197d87e486c417a2aa2da0b3a71c769
Moataz0000 <moatazfawzy150@gmail.com>
2025-08-18 11:22:37 +0300

    feat: reset password during send otp

--- apps/users/api/user/urls.py
+++ apps/users/api/user/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from apps.users.api.user.views import DocumentedTokenRefreshView
 from apps.users.api.user.views import LogoutAllDevicesView
 from apps.users.api.user.views import LogoutDeviceView
+from apps.users.api.user.views import SendOTPView
+from apps.users.api.user.views import SetPasswordView
 
 urlpatterns = [
     path(
@@ -14,4 +16,6 @@ urlpatterns = [
         "users/logout-all-devices/", LogoutAllDevicesView.as_view(), name="user-logout"
     ),
     path("users/logout-device/", LogoutDeviceView.as_view(), name="user-logout-device"),
+    path("users/reset-password/", SendOTPView.as_view(), name="reset-password"),
+    path("users/confirm/password", SetPasswordView.as_view(), name="verify_otp"),
 ]
