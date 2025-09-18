22e57f9af0ea49e5ca80fb7091dd4fe1a126592d
Moataz0000 <moatazfawzy150@gmail.com>
2025-09-18 20:21:37 +0300

    feat: updates

--- apps/location/admin/country/permissions.py
+++ apps/location/admin/country/permissions.py
@@ -20,7 +20,7 @@ class BaseCountryPermissions:
             ),
             CountryFields.NAME: FieldPermissions(
                 visible=(normal_admin),
-                editable=(normal_admin),
+                editable=(),
             ),
             CountryFields.NAME_AR: FieldPermissions(
                 visible=(normal_admin),
@@ -44,7 +44,7 @@ class BaseCountryPermissions:
             ),
             CountryFields.PHONE_CODE: FieldPermissions(
                 visible=(normal_admin),
-                editable=(normal_admin),
+                editable=(),
             ),
             CountryFields.APP_INSTALL_MONEY_INVITEE: FieldPermissions(
                 visible=(normal_admin),
