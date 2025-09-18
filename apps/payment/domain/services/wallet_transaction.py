06751dcdef532b11fa943561e26e5d9a648561ce
Moataz0000 <moatazfawzy150@gmail.com>
2025-09-18 19:33:49 +0300

    feat: created refarrel service

--- apps/payment/domain/services/wallet_transaction.py
+++ apps/payment/domain/services/wallet_transaction.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.core.files.base import File
 from djmoney.money import Money
 
+from apps.channel.constants import NotificationType
+from apps.channel.domain.services.notification import NotificationService
 from apps.payment.models.wallet import Wallet
 from apps.payment.models.wallet_transaction import WalletTransaction
 
@@ -16,16 +18,31 @@ class WalletTransactionService:
         transaction_note="SYSTEM ACTION",
         action_by: User | None = None,
         attachment: File | None = None,
+        notification_type: NotificationType | None = None,
     ) -> WalletTransaction:
         # [Important]: Update wallet balance directly with the amount (positive or negative)
         wallet.balance += amount
         wallet.save()
 
-        return WalletTransaction.objects.create(
+        transaction = WalletTransaction.objects.create(
             wallet=wallet,
             transaction_type=transaction_type,
-            amount=amount,
+            amount=amount,  # Positive for credit, negative for debit
             action_by=action_by,
             transaction_note=transaction_note,
             attachment=attachment,
         )
+
+        if amount > 0 and not notification_type:
+            notification_type = NotificationType.MONEY_ADDED
+
+        if notification_type:
+            NotificationService.create_action_notifications(
+                users=[wallet.user],
+                notification_type=notification_type,
+                send_fcm=True,
+                send_sms=False,
+                amount=str(amount),
+                new_balance=str(wallet.points_count),
+            )
+        return transaction
