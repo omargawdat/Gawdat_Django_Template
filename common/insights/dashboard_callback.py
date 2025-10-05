9ea4f9d727f18d15578662b0a0675c707817fcd2
Omar Gawdat <omargawdaat@gmail.com>
2025-10-05 19:12:20 +0200

    fix: linter error

--- common/insights/dashboard_callback.py
+++ common/insights/dashboard_callback.py
@@ -1,11 +1,14 @@
 import json
 from typing import Any
 
+from django.utils import timezone
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
+from djmoney.money import Money
 
+from apps.appInfo.models.contact_us import ContactUs
 from common.insights.helpers.chart import ChartHelper
 from common.insights.selectors.insight_selector import InsightSelector
-from apps.appInfo.models.contact_us import ContactUs
 
 
 def get_customer_growth_data():
