41a36c3fdf85611125c94d06d234d061e171c527
Moataz Fawzy <142129253+Moataz0000@users.noreply.github.com>
2025-11-16 15:06:31 +0200

    feat: created the endpoint (#191)
    * feat: created the endpoint
    
    * feat: updates
    
    * feat: updates

--- apps/appInfo/api/info/urls.py
+++ apps/appInfo/api/info/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from apps.appInfo.api.info.views import AppInfoAPIView
 from apps.appInfo.api.info.views import ContactUsCreateView
 from apps.appInfo.api.info.views import FAQListView
+from apps.appInfo.api.info.views import OnboardingAPIView
 from apps.appInfo.api.info.views import SocialAccountsAPIView
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path("info/", AppInfoAPIView.as_view(), name="app-info"),
     path("faqs/", FAQListView.as_view(), name="faqs"),
     path("contact-us/", ContactUsCreateView.as_view(), name="contact-us"),
+    path("onboarding/", OnboardingAPIView.as_view(), name="onboarding"),
 ]
--- apps/appInfo/api/info/views.py
+++ apps/appInfo/api/info/views.py
@@ -1,4 +1,5 @@
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,10 +8,12 @@ from rest_framework.views import APIView
 from apps.appInfo.api.info.serializers import AppInfoSerializer
 from apps.appInfo.api.info.serializers import ContactUsSerializer
 from apps.appInfo.api.info.serializers import FAQSerializer
+from apps.appInfo.api.info.serializers import OnBoardingSerializer
 from apps.appInfo.api.info.serializers import SocialAccountsSerializer
 from apps.appInfo.models.app_info import AppInfo
 from apps.appInfo.models.contact_us import ContactUs
 from apps.appInfo.models.faq import FAQ
+from apps.appInfo.models.onboarding import Onboarding
 from apps.appInfo.models.social import SocialAccount
 
 
@@ -85,3 +88,21 @@ class ContactUsCreateView(CreateAPIView):
             serializer.save(customer=self.request.user)
         else:
             serializer.save(customer=None)
+
+
+class OnboardingAPIView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    @extend_schema(
+        tags=["AppInfo/Onboarding"],
+        operation_id="listOnboarding",
+        responses={200: OnBoardingSerializer(many=True)},
+        description="Retrieve active onboarding screens",
+    )
+    def get(self, request):
+        onboardings = Onboarding.objects.filter(is_active=True)
+        onboarding_serializer = OnBoardingSerializer(
+            onboardings, many=True, context={"request": request}
+        ).data
+        return Response(onboarding_serializer, status=status.HTTP_200_OK)
