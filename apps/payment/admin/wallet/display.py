3f6f4129c1079238aa45fb69d4e55b0f9a27f71f
Moataz0000 <moatazfawzy150@gmail.com>
2025-11-17 12:15:31 +0200

    feat: wallet list view

--- apps/payment/admin/wallet/display.py
+++ apps/payment/admin/wallet/display.py
@@ -1,19 +1,30 @@
+from django.utils import timezone
+from django.utils.timesince import timesince
+from django.utils.translation import gettext_lazy as _
 from unfold.decorators import display
 
 from apps.payment.models.wallet import Wallet
 
 
 class WalletDisplayMixin:
-    @display(description="wallet", header=True)
+    @display(description=_("Customer"), header=True)
     def display_header(self, wallet: Wallet):
         """Display header with image if available."""
         return [
-            wallet.pk,
+            wallet.user.customer.email or wallet.user.customer.email,
             "",
             "O",
             {
-                "path": wallet.image.url
-                if hasattr(wallet, "image") and wallet.image
+                "path": wallet.user.customer.image
+                if wallet.user.customer.image
                 else None
             },
         ]
+
+    @display(description=_("Balance"), ordering="balance", label="info")
+    def display_balance(self, wallet: Wallet):
+        return f"{wallet.balance.amount} {wallet.balance.currency}"
+
+    @display(description=_("Last Update ago"), ordering="last_update", label="info")
+    def display_last_update(self, wallet: Wallet):
+        return f"{timesince(wallet.last_update, timezone.now())}"
